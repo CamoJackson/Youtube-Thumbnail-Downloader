db34bae961a946fc6406ecbc3a4bcb413d8c54b1
Jackson <jackson@borneman.com>
2017-02-21 08:58:13 -0500

    Added program Files

---
+++ YoutubeThumbDownloader.bat
@@ -0,0 +1,20 @@
+echo off
+SETLOCAL ENABLEEXTENSIONS
+SET me=%~n0
+SET parent=%~dp0
+
+set SAVELOCATION=C:\Users\%USERNAME%\Desktop\
+set REFFILE=C:\Users\%USERNAME%\Desktop\Ref.txt
+
+findstr /R /N "^" %REFFILE% | find /C ":" > TempFile
+set /p NUMIN=<TempFile
+:Loop
+findstr /r /n "." Ref.txt | findstr "%NUMIN%:" > TempFile
+set /p VIDID=<TempFile
+set VIDID=%VIDID:~2%
+set DOWNLOCATION= http://img.youtube.com/vi/%VIDID%/maxresdefault.jpg
+set LOCLOCATION=%SAVELOCATION%%VIDID%.jpg
+bitsadmin /transfer DownloadThumb /download %DOWNLOCATION% %LOCLOCATION%
+set /a NUMIN=%NUMIN%-1
+if %NUMIN% GTR 0 goto :Loop
+del TempFile
