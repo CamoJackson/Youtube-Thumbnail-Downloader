85e0046a98613525c0658c2a82932c34e4591951
Jackson <jackson@borneman.com>
2017-02-23 23:30:36 -0500

    Added if cases

--- YoutubeThumbDownloader.bat
+++ YoutubeThumbDownloader.bat
@@ -10,12 +10,36 @@ findstr /R /N "^" %REFFILE% | find /C ":" > TempFile
 set /p NUMIN=<TempFile
 :Loop
 findstr /r /n "." Ref.txt | findstr "%NUMIN%:" > TempFile
-set /p VIDID=<TempFile
+set VIDID=<TempFile
 
-:RmLnNum
-findstr /R "*:" %VIDID% 
-set VIDID=%VIDID:~1%
+if %NUMIN% GEQ 1000 (
+set NUMREM=5
+goto :NumDone
+)
 
+if %NUMIN% GEQ 100 (
+set NUMREM=4
+goto :NumDone
+)
+
+if %NUMIN% GEQ 10 (
+set NUMREM=3
+goto :NumDone
+)
+
+if %NUMIN% GEQ 1 (
+set NUMREM=2
+goto :NumDone
+)
+
+echo "ERROR: more than 1000 Video ID's"
+pause
+quit
+
+:NumDone
+Pause
+set VIDID=!VIDID:~%NUMREM%!
+Pause
 set DOWNLOCATION= http://img.youtube.com/vi/%VIDID%/maxresdefault.jpg
 set LOCLOCATION=%SAVELOCATION%%VIDID%.jpg
 bitsadmin /transfer DownloadThumb /download %DOWNLOCATION% %LOCLOCATION%
